422178e16cdaf205d5825ac9d20f36c53ec5c256
Suresh Shrestha <shrestha2lt8@gmail.com>
2019-02-23 16:24:21 +0545

    manually connect and disconnect mongodb connection

--- src/newsDbService.js
+++ src/newsDbService.js
@@ -2,32 +2,46 @@ const mongoose = require('mongoose');
 const { Article, Source } = require('./database/mongooseSchema');
 
 mongoose.promise = global.Promise;
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 module.exports = {
 	saveArticles: async (articles) => {
-		var articlesSaved = null;
+		mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+		var articlesSaved=null;
 		try {
-			await Article.insertMany(articles, { ordered: false });
-		} catch (error) {
-			if (error.code === 11000 || error.code === 11001) {
-				console.log('ignored duplicates')
-			} else {
+			const articlesSaved= await Article.insertMany( articles,{ordered:false});
+		} 
+		catch (error) {
+			if (error.code == 11000 || error.code == 11001)
+			{
+				console.log ("ignored duplicates")
+			}
+			else
+			{
 				console.log(error);
 			}
+			
 		}
+		mongoose.disconnect();
 		return articlesSaved;
+		
 	},
 	getArticles: async () => {
+		mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 		const newsArticles = await Article.find();
+		mongoose.disconnect();
 		return newsArticles;
 	},
 	getAllSources: async () => {
+		mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 		const sources = await Source.find();
+		mongoose.disconnect();
 		return sources;
 	},
 	isExist: async (newslink) => {
+		mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 		const count = await Article.count({ link: newslink });
-		return count > 0;
-	}
+		mongoose.disconnect();
+		return count > 0 ? true : false;
+	},
+
 };
