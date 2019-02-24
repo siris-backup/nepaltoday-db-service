c15a45620bc39d38ed51a6b064a51c545fa2cda4
Suraj Shrestha <syuraj@gmail.com>
2019-02-24 16:10:49 -0500

    Scripts needed if we need to recreate mongodb somewhere else

---
+++ src/scripts/source-data.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+
+// Insert News Sources
+db.sources.insert({ name: 'सेतोपाटी', link: 'https://setopati.com', logoLink: 'https://setopati.com/images/og-image.jpg' })
+db.sources.insert({ name: 'दैनिक नेपाल",', link: 'https://dainiknepal.com', logoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzh7lprLde3-7eyyxDMPVhfSJkKEHn6N7qDMz6_KwUpd6pC8U8' })
+db.sources.insert({ name: 'कान्तिपुर', link: 'https://kantipurdaily.com', logoLink: 'https://pbs.twimg.com/profile_images/943652378875535360/K81bnaWT_400x400.jpg' })
+db.sources.insert({ name: 'रातोपाटी', link: 'https://ratopati.com', logoLink: 'https://ratopati.prixa.net/static/normal/img/ratopati_default.jpg' })
