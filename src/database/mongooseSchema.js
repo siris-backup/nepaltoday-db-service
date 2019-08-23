6c435ce3e9243c46508f50ba175700e6b325e570
apple <apple@apples-MacBook-Pro.local>
2019-08-23 14:23:24 +0545

    twitter schema is added

--- src/database/mongooseSchema.js
+++ src/database/mongooseSchema.js
@@ -1,29 +1,48 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const Article = mongoose.model('Article', new Schema({
-	id: String,
-	title: { type: String, required: true },
-	link: { type: String, unique: true },
-	imageLink: { type: String, required: true },
-	isHeadline: Boolean,
-	shortDescription: String,
-	source: { type: mongoose.Schema.Types.ObjectId, ref: 'Source' },
-	hostIp: String,
-	publishedDate: { type: Date },
-	createdDate: { type: Date, default: Date.now },
-	modifiedDate: { type: Date, default: Date.now }
-}));
+const Article = mongoose.model(
+  "Article",
+  new Schema({
+    id: String,
+    title: { type: String, required: true },
+    link: { type: String, unique: true },
+    imageLink: { type: String, required: true },
+    isHeadline: Boolean,
+    shortDescription: String,
+    source: { type: mongoose.Schema.Types.ObjectId, ref: "Source" },
+    hostIp: String,
+    publishedDate: { type: Date },
+    createdDate: { type: Date, default: Date.now },
+    modifiedDate: { type: Date, default: Date.now }
+  })
+);
 
-const Source = mongoose.model('Source', new Schema({
-	id: String,
-	name: String,
-	link: String,
-	logoLink: String,
-	createdDate: { type: Date, default: Date.now },
-	modifiedDate: { type: Date, default: Date.now }
-}))
+const Source = mongoose.model(
+  "Source",
+  new Schema({
+    id: String,
+    name: String,
+    link: String,
+    logoLink: String,
+    createdDate: { type: Date, default: Date.now },
+    modifiedDate: { type: Date, default: Date.now }
+  })
+);
+
+const Twitter = mongoose.model(
+  "Twitter",
+  new Schema({
+    id: String,
+    name: String,
+    handle: String,
+    category: String,
+    weight: Number
+  })
+);
 
 module.exports = {
-	Article, Source
+  Article,
+  Source,
+  Twitter
 };
