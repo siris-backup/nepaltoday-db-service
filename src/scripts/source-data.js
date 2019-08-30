b0a08a1164cc8ed8a0327ca495d599796693afa9
shrijan <shrijan.siristechnology@gmail.com>
2019-08-30 14:57:23 +0545

    MODIFY-source-data: typo corrected

--- src/scripts/source-data.js
+++ src/scripts/source-data.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 
-// Insert News Sources
 db.sources.insert({
   name: 'सेतोपाटी',
   link: 'https://setopati.com',
@@ -25,10 +24,10 @@ db.sources.insert({
   ]
 })
 db.sources.insert({
-  name: 'दैनिक नेपाल",',
+  name: 'दैनिक नेपाल',
   link: 'https://dainiknepal.com',
   logoLink: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzh7lprLde3-7eyyxDMPVhfSJkKEHn6N7qDMz6_KwUpd6pC8U8',
-  ategory: [
+  category: [
     {
       name: 'news',
       path: '/section/latest-news'
@@ -45,7 +44,7 @@ db.sources.insert({
 })
 db.sources.insert({
   name: 'कान्तिपुर',
-  link: 'https://kantipurdaily.com',
+  link: 'https://ekantipur.com/',
   logoLink: 'https://pbs.twimg.com/profile_images/943652378875535360/K81bnaWT_400x400.jpg',
   category: [
     {
