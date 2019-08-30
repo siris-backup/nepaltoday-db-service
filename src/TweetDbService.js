b8967c46c3897a62a32f6929fd69295de373fa50
shrijan <shrijan.siristechnology@gmail.com>
2019-08-30 14:18:25 +0545

    MODIFY-source-data: conclict resolved

--- src/TweetDbService.js
+++ src/TweetDbService.js
@@ -1,46 +1,49 @@
 const { TwitterHandle, Tweet } = require('./database/mongooseSchema')
 
 module.exports = {
-	getTwitterHandles: async () => {
-		return TwitterHandle.find()
-	},
+  getTwitterHandles: async () => {
+    return TwitterHandle.find()
+  },
 
-	saveTwitterHandles: async tweets => {
-		var tweeterHandlers = null
-		try {
-			tweeterHandlers = await TwitterHandle.insertMany(tweets, {
-				ordered: false
-			})
-		} catch (error) {
-			if (error.code === 11000 || error.code === 11001) {
-				console.log('ignored duplicates')
-			} else {
-				console.log(error)
-			}
-		}
-		return tweeterHandlers
-	},
-	saveTweets: async tweets => {
-		var tweetSaved = null
-		try {
-			tweetSaved = await Tweet.insertMany(tweets, {
-				ordered: false
-			})
-		} catch (error) {
-			if (error.code === 11000 || error.code === 11001) {
-				console.log('ignored duplicates')
-			} else {
-				console.log(error)
-			}
-		}
-		return tweetSaved
-	},
+  saveTwitterHandles: async handles => {
+    try {
+      return await TwitterHandle.insertMany(handles, {
+        ordered: false
+      })
+    } catch (error) {
+      if (error.code === 11000 || error.code === 11001) {
+        console.log('ignored duplicates')
+      } else {
+        console.log(error)
+      }
+    }
+  },
 
-	deleteTweets: async conditions => {
-		return Tweet.deleteMany(conditions)
-	},
+  deleteTwitterHandles: async conditions => {
+    return TwitterHandle.deleteMany(conditions)
+  },
 
-	getTweets: async () => {
-		return Tweet.find()
-	}
+  saveTweets: async tweets => {
+    var tweetSaved = null
+    try {
+      tweetSaved = await Tweet.insertMany(tweets, {
+        ordered: false
+      })
+    } catch (error) {
+      if (error.code === 11000 || error.code === 11001) {
+        console.log('ignored duplicates')
+      } else {
+        console.log(error)
+      }
+    }
+    return tweetSaved
+  },
+
+  deleteTweets: async conditions => {
+    return Tweet.deleteMany(conditions)
+  },
+
+  getTweets: async () => {
+    return Tweet.find()
+  }
 }
--- src/scripts/twitter-handles.data.js
+++ src/scripts/twitter-handles.data.js
@@ -1,32 +1,9 @@
-const { en } = require('../lang/en')
-const { TwitterHandle } = require('../database/mongooseSchema')
-const { getTwitterHandles, saveTwitterHandles } = require('../TweetDbService')
-
 /* eslint-disable no-undef */
-const { TECH } = en.category
-const handles = [
-	{
-		name: 'Shrjan',
-		handle: '@itsmeshrijan',
-		category: TECH,
-		weight: 20
-	}
-]
-;(async function () {
-	try {
-		const initialHandles = await getTwitterHandles()
-		if (!initialHandles) {
-			const savedHandles = await saveTwitterHandles(handles)
-			console.log('saved handles', savedHandles)
-		} else {
-			console.log('Handles already exist data are ==', initialHandles)
-		}
-	} catch (error) {
-		if (error.code === 11000 || error.code === 11001) {
-			console.log('ignored duplicates')
-		} else {
-			console.log(error)
-		}
-	}
-	return null
-})()
+
+// Insert News Sources
+db.twitterhandles.insert({
+  handle: '@itsmeshrijan',
+  name: 'Shrijan',
+  weight: 20,
+  category: 'TECHNOLOGY'
+})
