4a2457e017809d74159b407b4de90641a924f85c
shrijan <shrijan.siristechnology@gmail.com>
2019-11-15 09:53:03 +0545

    NEP-149/fetch-one-single-news: jest spyon the find method is implemented but cant mock for lean method

--- src/UserDbService.test.js
+++ src/UserDbService.test.js
@@ -1,7 +1,10 @@
 const { User } = require('./database/mongooseSchema')
+// const { getUsers } = require('./UserDbService')
 
 describe('User Db service ', () => {
 	it('get all user from db', async () => {
-		jest.spyOn(User, 'find').mockImplementation(() => {})
+		jest.spyOn(User, 'find').mockImplementationOnce(() => {})
+		// await getUsers()
+		// expect(findSpy).toHaveReturned()
 	})
 })
